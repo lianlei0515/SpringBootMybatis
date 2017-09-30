9fdb17b770f56b9fef1b7f75bd3652953a80d9e8
tony lian <tony.lian@chinaventure.com.cn>
2017-09-30 14:00:45 +0800

    添加工具类

---
+++ src/main/java/com/example/demo/utils/restMessage/RestCode.java
@@ -0,0 +1,39 @@
+package com.example.demo.utils.restMessage;
+
+/**
+ * Created by Tony on 2017/9/30.
+ * com.example.demo.utils.restMessage.SpringBootMybatis
+ */
+public class RestCode {
+    //成功
+    public final static int SUCCESSCODE = 0;
+    //参数错误
+    public final static int PARAMETERERROR = 4004;
+    //序列化错误
+    public final static int SERIALIZATIONERROR = 4005;
+    //该方法操作失败
+    public final static int OPERATIONFAILED = 5001;
+    //没有数据
+    public final static int NODATA = 4005;
+
+    public final static String SUCCESSMESSAGE_CN = "保存成功";
+
+    public final static String QUERYWASSUCCESSFUL_CN = "查询成功";
+
+    public final static String DELETESUCCESSFULLY_CN = "删除成功";
+
+    public final static String MODIFYSUCCESSFULLY_CN = "修改成功";
+
+    public final static String SAVEFAILED_CN = "保存失败";
+
+    public final static String STRINGMODIFYFAILED_CN = "修改失败";
+
+    public final static String DELETEFAILED_CN = "删除失败";
+
+    public final static String QUERYFAILED_CN = "查询失败";
+
+    public final static String NODATA_CN = "没有数据";
+
+    public final static String SORRY_CN = "对不起！";
+
+}
